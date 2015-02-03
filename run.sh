6e8e5bf7a578d6764bf6ecbcc1b3f3f8fcab1da4
dajevu <jdavis@zazarie.com>
2015-02-03 16:41:35 -0700

    Files

--- run.sh
+++ run.sh
@@ -2,17 +2,15 @@
 
 echo `env`
 
-git pull
-
-# start mongo db
-nohup mongod --rest --httpinterface --smallfiles &
-
-# wait for mongo to load
-sleep 30
-
 cd /local/git/docker-mongo
 git pull
 
+# start mongo db
+mongod --fork --logpath /tmp/mongodb.log --rest --httpinterface --smallfiles
+
+# wait for mongo to load
+#sleep 30
+
 # load demo data
-cd /local/git/docker-mongo/northwind-mongo-master/
-./mongo-import.sh
+#cd /local/git/docker-mongo/northwind-mongo-master/
+#./mongo-import.sh
